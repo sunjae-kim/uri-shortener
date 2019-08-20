61399db09f7b544e26217e146438b2de1a3ac000
sunjae-kim <kimsj9484@gmail.com>
2019-08-20 16:17:00 +0900

    Doesn't check token when use service

--- functions/src/routes/index.js
+++ functions/src/routes/index.js
@@ -3,8 +3,8 @@ const route = express.Router();
 const shortener = require('./shortener');
 const { checkToken } = require('../middleware');
 
-route.get('/', checkToken, shortener.getList);
-route.get('/:path', checkToken, shortener.toShort);
+route.get('/', shortener.getList);
+route.get('/:path', shortener.toShort);
 route.post('/shorten', checkToken, shortener.shorten);
 route.delete('/:path', checkToken, shortener.deleteShort);
 
