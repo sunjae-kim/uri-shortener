fec51e1f162d6da59fe731955a884bf0a7f9f7cd
sunjae-kim <kimsj9484@gmail.com>
2019-08-20 15:46:39 +0900

    Fix minor issues

--- functions/src/database.js
+++ functions/src/database.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const admin = require('firebase-admin');
 
-const serviceAccount = require(process.env.FIREBASE_KEY_PATH);
+const serviceAccount = require('../service_account.json');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: process.env.FIREBASE_DATABASE_URL,
