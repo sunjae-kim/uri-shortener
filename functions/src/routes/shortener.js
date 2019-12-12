53ee32f4881c41d2f8d123de5694ff7aed3a6109
sunjae-kim <kimsj9484@gmail.com>
2019-12-12 23:43:54 +0900

    Prevent shorten tisha.me uri from server
     - Prevent shortening uri that responses tisha.me
     - Validate keyword before redirect

--- functions/src/routes/shortener.js
+++ functions/src/routes/shortener.js
@@ -4,11 +4,15 @@ const { validateShortData } = require('../utils/validators');
 const wrapper = require('../common/wrapper');
 
 const redirect = wrapper(async (req, res) => {
-  const domain = process.env.CLIENT_DOMAIN;
-  const { keyword } = req.params;
+  const { value, error } = validateShortData(req.params);
+  if (error) return res.status(400).send({ message: error.details[0].message });
+
+  const { keyword } = value;
   const snapshot = await shortListRef.child(keyword).once('value');
   const data = snapshot.val();
   if (data) return res.redirect(301, data.originalUri);
+
+  const domain = process.env.CLIENT_DOMAIN;
   return res.redirect(`${domain}/invalid?keyword=${keyword}`);
 });
 
@@ -24,11 +28,15 @@ const validateShort = wrapper(async (req, res) => {
   const httpsRegex = /^https:\/\//;
   const isHttps = httpsRegex.test(originalUri);
   const get = isHttps ? https.get : http.get;
-  return get(originalUri, response =>
-    res.status(200).send({
-      originalUri: response.responseUrl,
-      keyword,
-    }),
+
+  return get(originalUri, response => {
+    const { hostname } = new URL(response.responseUrl);
+    const domain = process.env.CLIENT_DOMAIN;
+    if (domain.includes(hostname))
+      return res.status(400).send({ message: '등록할 수 없는 URI 입니다' });
+    return res.status(200).send({ originalUri, keyword });
+  }).on('error', () =>
+    res.status(400).send({ message: '유효하지 않은 URI 입니다' }),
   );
 });
 
--- functions/src/utils/validators.js
+++ functions/src/utils/validators.js
@@ -4,14 +4,12 @@ const validateShortData = short => {
   const schema = {
     originalUri: Joi.string()
       .uri()
-      .required()
       .error(() => ({
         message: '올바른 URI 형식이 아닙니다',
       })),
     keyword: Joi.string()
-      .required()
-      .trim()
       .regex(/^[^.[\]#$ ]+$/)
+      .trim()
       .error(() => ({
         message: '키워드에 . [ ] # $ 및 공백 문자는 사용하실 수 없습니다',
       })),
